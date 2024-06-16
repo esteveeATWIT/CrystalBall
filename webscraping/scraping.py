2a23adf5e8cee9c89e7146d9a14f80ddcf3a8a01
Enrique Esteve <estevee@wit.edu>
2024-06-16 18:52:37 -0400

    Scraping setup complete. Need to work on database connection now

--- webscraping/scraping.py
+++ webscraping/scraping.py
@@ -1,7 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+import time
 import pandas as pd
 import re
 import requests
 from bs4 import BeautifulSoup
+
 
 '''
 def create_connection():
@@ -18,9 +25,7 @@ def create_connection():
         print("Error while connecting to MySQL", e)
     return None
     
-'''
-'''
-# Function to insert data into the database
+
 def insert_team_stats(connection, teamID, name, games_played, games_won, ERA, OBP, Whip, Slg):
     cursor = connection.cursor()
     insert_query = """
@@ -31,58 +36,49 @@ def insert_team_stats(connection, teamID, name, games_played, games_won, ERA, OB
     cursor.execute(insert_query, (games_played, games_won, ERA, OBP, Whip, Slg, teamID))
     connection.commit()
 '''
-# Web scraping setup
-def scrape_batting_stats():
-    url = 'https://www.espn.com/mlb/stats/team' 
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+def scrape_teamids(): #This will get the names and put it on a list
+    url = 'https://www.espn.com/mlb/stats/team/_/table/batting/sort/gamesPlayed/dir/desc' 
 
-    table = soup.find('table', attrs={'class': 'Table Table'}) 
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver.get(url)
+    
+    time.sleep(5)
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    driver.quit() 
+    
+
+    table = soup.find('table', attrs={'class': 'Table Table--align-right Table--fixed Table--fixed-left'})
     if not table:
-        print("Table not found")
+        print("Table with names not found")
         return []
-    rows = table.find_all('tr')
-
-    batting_stats = []
-
-    for row in rows[1:]: 
-        cols = row.find_all('td')
-        teamID = cols[0].text.strip()[:3]
-        name = cols[0].text.strip()
-        OBP = float(cols[13].text.strip())  
-        SLG = float(cols[14].text.strip())  
-
-        batting_stats.append((teamID, name, OBP, SLG))
     
-    return batting_stats
-
-def scrape_pitching_stats():
-    url = 'https://www.espn.com/mlb/stats/team/_/view/pitching'  
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    table = soup.find('table') 
     rows = table.find_all('tr')
+    teamslist = []
 
-    pitching_stats = []
-
-    for row in rows[1:]: 
+    for row in rows[1:]:
         cols = row.find_all('td')
-        teamID = cols[0].text.strip()[:3]
-        name = cols[0].text.strip()
-        games_played = int(cols[1].text.strip())
-        games_won = int(cols[2].text.strip())
-        ERA = float(cols[3].text.strip())
-        Whip = float(cols[6].text.strip()) 
 
-        pitching_stats.append((teamID, name, games_played, games_won, ERA, Whip))
-    
-    return pitching_stats
+        if len(cols) == 0:
+          print("Empty Row or no <td> elements found")
+          continue
 
+        img_tag = cols[1].find('img', attrs={'class' : 'Image Logo Logo__sm'})
+        if img_tag:
+            if 'title' in img_tag.attrs:
+                team_name = img_tag['title'].strip()
+                teamId = team_name[:3].upper() 
+                teamslist.append((teamId, team_name))
+            else:
+                print ("Title attribute not found in img tag")
+        else:
+            print("Img tag not found")
 
+    return teamslist
 
-def main():
-    url = 'https://www.espn.com/mlb/stats/team' 
+def scrape_batting_stats(): #grabbing the batting stats needed for algorithm and adding to list
+    url = 'https://www.espn.com/mlb/stats/team/_/table/batting/sort/gamesPlayed/dir/desc' 
+
     #header for bypassing the website so we can connect succesfully (makes the request look like it's coming from a real browser)
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
@@ -107,31 +103,98 @@ def main():
         return []
     rows = table.find_all('tr')
 
-    testing = []
+    batting_stats = []
 
     for row in rows[1:]: 
         cols = row.find_all('td')
-        teamID = cols[0].text.strip()[:3]
-        name = cols[0].text.strip()
         OBP = float(cols[13].text.strip())  
         SLG = float(cols[14].text.strip())  
+        batting_stats.append((OBP, SLG))
 
-        testing.append((teamID, name, OBP, SLG))
+    return (batting_stats) 
+
+def scrape_pitching_stats(): #grabbing pitching stats and putting on list
+    url = 'https://www.espn.com/mlb/stats/team/_/view/pitching/table/pitching/sort/gamesPlayed/dir/desc'  
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        print("Successfully connected to the website")
+    else:
+        print(f"Failed to connect to the website. Status code: {response.status_code}")
+        return
     
-    print (testing) 
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    if not soup:
+        print("Failed to parse the content")
+        return
+
+    table = soup.find('table', attrs={'class': 'Table Table--align-right'}) 
+    if not table:
+        print("Table not found")
+        return []
+    rows = table.find_all('tr')
+
+    pitching_stats = []
+
+    for row in rows[1:]: 
+        cols = row.find_all('td')
+        games_played = int(cols[0].text.strip())
+        games_won = int(cols[1].text.strip())
+        ERA = float(cols[3].text.strip())
+        Whip = float(cols[15].text.strip()) 
+        pitching_stats.append(( games_played, games_won, ERA, Whip))
+    
+
+    return pitching_stats
+
+def combine_stats(team_ids, batting_stats, pitching_stats):
+    combined_stats = []
+    for i in range(len(team_ids)):
+        combined_stats.append((
+            team_ids[i][0],  # teamId
+            team_ids[i][1],  # teamName
+            pitching_stats[i][0],  # games_played
+            pitching_stats[i][1],  # games_won
+            pitching_stats[i][2],  # ERA
+            batting_stats[i][0],  # OBP
+            pitching_stats[i][3],  # WHIP
+            batting_stats[i][1]  # SLG
+        ))
+    return combined_stats
+
+
+def main():
+
+    pitching_stats = scrape_pitching_stats()
+    batting_stats = scrape_batting_stats()
+    team_ids = scrape_teamids()
+
+    combined_stats = combine_stats(team_ids, batting_stats, pitching_stats)
+    print("\nCombined Stats:")
+    for stats in combined_stats:
+        print(stats)
+
 '''
     connection = create_connection()
     if connection:
+        teamslist = scrape_teamslist()
         batting_stats = scrape_batting_stats()
         pitching_stats = scrape_pitching_stats()
 
         combined_stats = {}
+        for team in teamslist:
+            teamID, name = team
+            combined_stats[teamID] = {'name': name}
         for team in batting_stats:
-            teamID, name, OBP, SLG = team
-            combined_stats[teamID] = {'name': name, 'OBP': OBP, 'SLG': SLG}
+            OBP, SLG = team
+            combined_stats[teamID] = {'OBP': OBP, 'SLG': SLG}
         
         for team in pitching_stats:
-            teamID, name, games_played, games_won, ERA, Whip = team
+            games_played, games_won, ERA, Whip = team
             if teamID in combined_stats:
                 combined_stats[teamID].update({
                     'games_played': games_played,
